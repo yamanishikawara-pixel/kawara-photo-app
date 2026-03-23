9133abf6ea023100e8c159173974613e4b2f8186
Yamanishi <yamanishi@example.com>
2026-03-23 11:02:03 +0900

    改良

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -229,7 +229,7 @@ export default function MapPage() {
           canvas.height = viewport.height;
 
           if (context) {
-            await page.render({ canvasContext: context, viewport: viewport }).promise;
+            await page.render({ canvasContext: context, viewport: viewport } as any).promise;
             const blob = await new Promise<Blob | null>((resolve) => canvas.toBlob(resolve, 'image/jpeg', 0.9));
             if (blob) {
               fileToUpload = blob;
